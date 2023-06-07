ddd85c4f46a858232bb503044dd05e7041e04155
Felipe <felipebarrera.gbp@gmail.com>
2023-06-07 10:11:21 -0500

    Maquetacion del proyecto con pinia

---
+++ src/router/index.js
@@ -0,0 +1,35 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../pages/HomeView.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView,
+      children: [
+        {
+          path: ':id(\\d+)',
+          component: () => import('../pages/MessagesView.vue')
+        },
+        {
+          path: ':(.*)',
+          component: () => import('../pages/VoidMessagesView.vue')
+        }
+      ]
+    },
+    {
+      path: '/profile',
+      name: 'profile',
+      component: () => import('../pages/ProfileView.vue')
+    },
+    {
+      path: '/about',
+      name: 'about',
+      component: () => import('../pages/AboutView.vue')
+    }
+  ]
+})
+
+export default router
