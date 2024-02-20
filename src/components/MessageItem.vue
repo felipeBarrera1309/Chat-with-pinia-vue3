596b72ebc76502a0880233fcece8c29400b03f3c
felipeBarrera <felipebarrera.gbp@gmail.com>
2024-02-19 20:52:06 -0500

    poderosisisimamaaaaa NS-200

--- src/components/MessageItem.vue
+++ src/components/MessageItem.vue
@@ -6,6 +6,7 @@
     </div>
     <div class="content">
       <div class="details" :class="{ self: isSelf }">
+        <p>{{ 'NS' }}</p>
         <h5>{{ author }}</h5>
         <p>{{ time }}</p>
       </div>
